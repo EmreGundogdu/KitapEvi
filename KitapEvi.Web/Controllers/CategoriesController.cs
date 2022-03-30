e5060c690420b422973751e57501e282e3c5b2b8
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-03-30 23:21:32 +0300

    added Categories view

---
+++ KitapEvi.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using KitapEvi.DataAccess.Data;
+using KitapEvi.Model.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KitapEvi.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        readonly ApplicationDbContext _context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categories = _context.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
