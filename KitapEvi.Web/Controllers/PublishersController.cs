f847da78bf0dc124071da7b087e512b510e15353
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-03-31 23:56:54 +0300

    added Controllers & Views

---
+++ KitapEvi.Web/Controllers/PublishersController.cs
@@ -0,0 +1,38 @@
+﻿using KitapEvi.DataAccess.Data;
+using KitapEvi.Model.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KitapEvi.Web.Controllers
+{
+    public class PublishersController : Controller
+    {
+        readonly ApplicationDbContext _context;
+
+        public PublishersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult UpdateInsert(int? id)
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UpdateInsert()
+        {
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ KitapEvi.Web/Controllers/WritersController.cs
@@ -0,0 +1,37 @@
+﻿using KitapEvi.DataAccess.Data;
+using KitapEvi.Model.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KitapEvi.Web.Controllers
+{
+    public class WritersController : Controller
+    {
+        readonly ApplicationDbContext _context;
+
+        public WritersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult UpdateInsert(int? id)
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UpdateInsert()
+        {
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            return RedirectToAction(nameof(Index));
+
+        }
